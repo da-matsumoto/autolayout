3a51a37075d26aad183d1dc6a73e99c82892ebc6
GS05-44-MatsumotoDaisuke <da.mats1990@gmail.com>
2017-08-22 20:28:57 +0900

    autolayout基本

--- constraint/ViewController.swift
+++ constraint/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var button: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let topConstraint = NSLayoutConstraint(item: button, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 130.0)
+    let leftConstraint = NSLayoutConstraint(item: button, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 10.0)
+    view.addConstraints([topConstraint, leftConstraint])
+    topConstraint.isActive = true
+    leftConstraint.isActive = true
+    
+    
   }
 
   override func didReceiveMemoryWarning() {
